81a0533d1b3fe488eda0d1babf43c1a6c672b200
gabwestside <gabrielnfl13@gmail.com>
2021-12-29 16:11:01 -0300

    Update 1.0.1

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SalesWebMVC.Models;
+using SalesWebMVC.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
